8a87bd3c93a2867d382dd9cdc289b059dc41fb0e
CCXT Backtrader Developer <developer@ccxt-backtrader.com>
2026-09-08 23:16:29 +0800

    chore: release v0.2.0

--- src/solidrock/__init__.py
+++ src/solidrock/__init__.py
@@ -30,7 +30,7 @@ from solidrock.data.symbols import AssetType, Symbol, parse_symbol
 from solidrock.factors import Factor, FactorData, analyze_factor
 from solidrock.strategy.base import Strategy
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.2.0"
 
 __all__ = [
     "AssetType",
